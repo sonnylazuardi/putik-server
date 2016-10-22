1a5e10d19d0ffbdccb3f5309e6c97e7727250a6f
dannych <dnny.ch@gmail.com>
2016-10-22 10:15:53 +0700

    add categories path

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const FIREBASE_URL = 'https://petik-b0273.firebaseio.com';
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,9 +9,26 @@ const app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', (req, res) => {
-    res.send('PUTIK is live');
+    res.send('Putik ROCKS!!!');
+})
+
+app.get('/categories', (req, res) => {
+    axios.get(FIREBASE_URL + '/categories.json')
+        .then((data) => {
+            let keys = Object.keys(data.data);
+            res.json(keys.map((k) => data.data[k]));
+        })
+        .catch((error) => console.log(error));
 });
 
 app.listen(app.get('port'), () => {
     console.log('PUTIK will rock at ' + app.get('port'));
 });
+
+function addCategory(db, category) {
+    db.ref('/categories').push({
+        name: 'Hello',
+        genre: 'top50',
+        cover: 'asdasd'
+    });
+}
