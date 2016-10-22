0f62724278f180ab0e6de989680f9cc4aef497ba
dannych <dnny.ch@gmail.com>
2016-10-22 13:42:26 +0700

    refactor

--- seeds/playlist.js
+++
@@ -1,9 +0,0 @@
-const FIREBASE_URL = 'https://petik-b0273.firebaseio.com';
-
-const axios = require('axios');
-
-axios.post(FIREBASE_URL + '/playlists.json', {
-    name: 'test',
-    genre: 'test',
-    cover: 'test'
-});
